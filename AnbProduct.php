d2e302937379495dc4506087e0fea476dbc5bd06
Imran <imran@zeropoint.it>
2017-06-21 12:00:06 +0500

    Preserving code.

--- AnbProduct.php
+++ AnbProduct.php
@@ -10,7 +10,8 @@ namespace AnbTopDeals;
 
 use AnbApiClient\Aanbieders;
 
-class AnbProduct {
+class AnbProduct
+{
 
     public $crmApiEndpoint = "http://api.econtract.be/";//Better to take it from Admin settings
     public $anbApi;
@@ -25,25 +26,31 @@ class AnbProduct {
         $this->anbApi = wpal_create_instance(Aanbieders::class, [$this->apiConf]);
     }
 
-    function topDealProducts( $atts ) {
-        $atts = shortcode_atts( array(
+    function topDealProducts($atts, $nav="")
+    {
+        $atts = shortcode_atts(array(
             'cat' => '',
-            'detaillevel' => 'supplier',//specifications
+            'detaillevel' => [],//specifications, logo
             'sg' => 'consumer',
             'product_1' => [],
             'product_2' => [],
             'product_3' => [],
-            'lang' => 'nl'
+            'lang' => 'nl',
+            'is_active' => 0
 
-        ), $atts, 'anb_top_deal_products' );
+        ), $atts, 'anb_top_deal_products');
 
-        if(!empty($atts['detaillevel'])) {
+        if (!empty($atts['detaillevel'])) {
             $atts['detaillevel'] = explode(',', $atts['detaillevel']);
         }
 
-        if(empty($atts['product_1']) || empty($atts['product_2']) || empty($atts['product_3'])) {
+        if (empty($atts['product_1']) || empty($atts['product_2']) || empty($atts['product_3']) || empty($nav)) {
             return;
         }
+
+        $nav = sanitize_text_field($nav);
+
+        pll_register_string($nav, $nav, 'AnbTopDeals');
 
         //remove empty params
         $params = array_filter($atts);
@@ -63,13 +70,19 @@ class AnbProduct {
 
         $cats = array_unique($cats);
 
-        $products = $this->anbApi->getProducts(array('cat'=>$cats, 'sg'=>$atts['sg'], 'lang'=>$atts['lang'],
-            'productid'=>array($atts['product_1'], $atts['product_2'], $atts['product_3']),
-            'detaillevel'=>$atts['detaillevel']));
+        $products = $this->anbApi->getProducts(array('cat' => $cats, 'sg' => $atts['sg'], 'lang' => $atts['lang'],
+            'productid' => array($atts['product_1'], $atts['product_2'], $atts['product_3']),
+            'detaillevel' => $atts['detaillevel']));
 
-        echo "<pre>PRODUCTS>>>";
+        /*echo "<pre>PRODUCTS>>>";
         print_r($products);
-        echo "</pre>";
+        echo "</pre>";*/
+
+        $class = '';
+        /*if($atts['is_active'] == 1) {
+            $class = "<div class='{$class}'>";
+        }*/
+        //Write here jQuery code to manipulate the navigation, because there is a chance that some content is already rendered
 
         return "<pre>" . print_r($params, true) . "<br><br>" . print_r($products, true) . "</pre>";
     }
