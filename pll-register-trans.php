afd6e1f8e0f3e8e8ed12f823a1a60d773d91393f
Imran <imran@zeropoint.it>
2017-07-18 17:01:10 +0500

    Translations are now part of the plugin.

--- pll-register-trans.php
+++ pll-register-trans.php
@@ -9,6 +9,19 @@ if ( !function_exists( 'pll_register_string' ) ) {
     require_once WP_PLUGIN_DIR . '/polylang/include/api.php';
 }
 
-pll_register_string('Proximus most popular', 'Proximus most popular', 'AnbTopDeals');
-pll_register_string('Installation', 'Installation', 'AnbTopDeals');
-pll_register_string('Activation', 'Activation', 'AnbTopDeals');
+function anbTopDealsTrans() {
+    pll_register_string('Most popular', 'Most popular', 'AnbTopDeals');
+    pll_register_string('Installation', 'Installation', 'AnbTopDeals');
+    pll_register_string('Activation', 'Activation', 'AnbTopDeals');
+    pll_register_string('Info and options', 'Info and options', 'AnbTopDeals');
+    pll_register_string('Free installation', 'Free installation', 'AnbTopDeals');
+    pll_register_string('Free activation', 'Free activation', 'AnbTopDeals');
+    pll_register_string('t.w.v', 't.w.v', 'AnbTopDeals');
+    pll_register_string('BEST', 'BEST', 'AnbTopDeals');
+    pll_register_string('Promo', 'Promo', 'AnbTopDeals');
+    pll_register_string('Offer', 'Offer', 'AnbTopDeals');
+    pll_register_string('advantage', 'advantage', 'AnbTopDeals');
+    pll_register_string('the first %d months', 'the first %d months', 'AnbTopDeals');
+}
+
+add_action('init', 'anbTopDealsTrans');
