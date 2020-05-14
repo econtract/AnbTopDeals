d4c361d6e5219597d30747d10d9147703e9e1056
Gieljan Koole <gieljan@letstalk.nl>
2020-05-14 13:49:26 +0200

    AANB-239 | Top deals: use new function and revert old changes

--- pll-register-trans.php
+++ pll-register-trans.php
@@ -25,6 +25,24 @@ function anbTopDealsTrans() {
     pll_register_string('Offer', 'Offer', 'AnbTopDeals');
     pll_register_string('advantage', 'advantage', 'AnbTopDeals');
     pll_register_string('the first %d months', 'the first %d months', 'AnbTopDeals');
+
+    pll_register_string('per year','per year', 'AnbTopDeals');
+    pll_register_string('Top 5 cheapest rates', 'Top 5 cheapest rates', 'AnbTopDeals');
+    pll_register_string('Quickly view the details of our cheapest suppliers and rates.', 'Quickly view the details of our cheapest suppliers and rates.','AnbTopDeals' );
+    pll_register_string('Discount', 'Discount', 'AnbTopDeals');
+    pll_register_string('Send us your invoice', 'Send us your invoice', 'AnbTopDeals');
+    pll_register_string('We are Aanbieders', 'We are Aanbieders', 'AnbTopDeals');
+    pll_register_string('100% independent', '100% independent', 'AnbTopDeals');
+    pll_register_string('100% free', '100% free','AnbTopDeals');
+    pll_register_string('Easy to switch', 'Easy to switch', 'AnbTopDeals');
+    pll_register_string('Everything is taking care of', 'Everything is taking care of', 'AnbTopDeals');
+    pll_register_string('Guarantee up to date','Guarantee up to date', 'AnbTopDeals');
+    pll_register_string('Show us your invoice','Show us your invoice', 'AnbTopDeals');
+    pll_register_string('Customer score','Customer score', 'AnbTopDeals');
+    pll_register_string('Show us your invoice','Show us your invoice', 'AnbTopDeals');
+    //pll_register_string('A score of '. $reviewFormatted. ' out of  5 based on ' . $reviewCount . ' reviews on Google','A score of '. $reviewFormatted. ' out of  5 based on ' . $reviewCount . ' reviews on Google', 'AnbTopDeals');
+
+
 }
 
 add_action('init', 'anbTopDealsTrans');
