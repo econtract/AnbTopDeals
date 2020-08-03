469a1535340abee432f9b48009e88b1db9d5c9d2
Sjef van Doorn <sjef@letstalk.nl>
2020-08-03 17:27:00 +0200

    AANB-367 | Topdeals: changed and removed filterDeals and dealsTable classes to tabs, commented redundant filterDeals

--- js/top-deals.js
+++ js/top-deals.js
@@ -1,28 +1,29 @@
-/**
- * Created by imran on 6/21/17.
- */
-
-function appendToSelector(selector, html) {
-    jQuery(selector).append(html).promise().done(function () {
-        if(selector === ".topDeals .dealsTable"){
-            fixDealsTableHeight(jQuery('.dealsTable.topDealsTable'));
-        }
-    });
-}
-
-function appendNav(selector, html) {
-    jQuery(selector).append(html);
-}
-
-jQuery(document).ready(function($){
-    $('.topDeals .filterDeals ul li a').on('click', function() {
-        $('.topDeals .filterDeals ul li.active').removeClass('active');//remove previous active class
-
-        //hide all rows now
-        $('.topDeals .dealsTable .row').hide();
-
-        //show the clicked one
-        $(this).parents('li').addClass('active');
-        $('.row.'+$(this).attr('related')).show();
-    });
-});
+// TODO: Cleanup if not used (lt-restyle)
+// /**
+//  * Created by imran on 6/21/17.
+//  */
+//
+// function appendToSelector(selector, html) {
+//     jQuery(selector).append(html).promise().done(function () {
+//         if(selector === ".topDeals .dealsTable"){
+//             fixDealsTableHeight(jQuery('.dealsTable.topDealsTable'));
+//         }
+//     });
+// }
+//
+// function appendNav(selector, html) {
+//     jQuery(selector).append(html);
+// }
+//
+// jQuery(document).ready(function($){
+//     $('.topDeals .filterDeals ul li a').on('click', function() {
+//         $('.topDeals .filterDeals ul li.active').removeClass('active');//remove previous active class
+//
+//         //hide all rows now
+//         $('.topDeals .dealsTable .row').hide();
+//
+//         //show the clicked one
+//         $(this).parents('li').addClass('active');
+//         $('.row.'+$(this).attr('related')).show();
+//     });
+// });
